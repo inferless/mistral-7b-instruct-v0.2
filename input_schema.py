fe50406d506bb7e860588c06cfcf40b334eea1e8
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-04-08 13:40:45 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -3,6 +3,36 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["There is a fine house in the forest"]
+        'example': ["What is deep learning?"]
+    },
+    "temperature": {
+        'datatype': 'FP32',
+        'required': False,
+        'shape': [1],
+        'example': [0.7]
+    },
+    "top_p": {
+        'datatype': 'FP32',
+        'required': False,
+        'shape': [1],
+        'example': [0.1]
+    },
+    "repetition_penalty": {
+        'datatype': 'FP32',
+        'required': False,
+        'shape': [1],
+        'example': [1.18]
+    },
+    "max_tokens": {
+        'datatype': 'INT16',
+        'required': False,
+        'shape': [1],
+        'example': [256]
+    },
+    "top_k":{
+        'datatype': 'INT8',
+        'required': False,
+        'shape': [1],
+        'example': [40]
     }
 }
